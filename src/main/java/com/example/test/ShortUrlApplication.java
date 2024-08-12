eb85d90da6d4edbbe15c5070a858ac222cae95d5
minho <pmmh9395@gmail.com>
2024-08-12 10:52:06 +0900

    Shorten Url API finished

--- src/main/java/com/example/test/ShortUrlApplication.java
+++ src/main/java/com/example/test/ShortUrlApplication.java
@@ -8,6 +8,6 @@ public class ShortUrlApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ShortUrlApplication.class, args);
+		System.out.println("/shortURl finished");
 	}
-
 }
